fbf0ae49b1f17bf6526f30c307d1c48a1c957c37
Justin Hong <jhong2009@gmail.com>
2018-04-12 22:56:05 -0700

    created MovieCreate.js and set up MovieCreate class

---
+++ client/src/Movies/MovieCreate.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class MovieCreate extends React.Component {
+	constructor(props) {
+		super();
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return ();
+	}
+}
