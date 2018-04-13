2a40fbd721862502e1b1087f4c249b70705101b9
Justin Hong <jhong2009@gmail.com>
2018-04-12 22:48:16 -0700

    in App.js: implemented Switch for urls that do not exist

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,43 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       savedList: []
-    }
+    };
   }
 
-  addToSavedList = (movie) => {
-    console.log(this.state.savedList)
+  addToSavedList = movie => {
+    console.log(this.state.savedList);
     const savedList = this.state.savedList;
     savedList.push(movie);
-    this.setState({savedList});
-  }
+    this.setState({ savedList });
+  };
 
-  render(){
+  render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" render={ (props) => {
-          return(<Movie {...props} addToSavedList={this.addToSavedList}/>)
-        }} />
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route
+            path="/movies/:id"
+            render={props => {
+              return <Movie {...props} addToSavedList={this.addToSavedList} />;
+            }}
+          />
+          <Route
+            component={() => {
+              return <h1>Whoa! Where do you think you're going?</h1>;
+            }}
+          />
+        </Switch>
       </div>
-    )
+    );
   }
 }
