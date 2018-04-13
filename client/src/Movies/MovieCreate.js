e87fda1ad2ac1590a21be953a99057b905536b02
Justin Hong <jhong2009@gmail.com>
2018-04-12 22:58:37 -0700

    in MovieCreate.js: set up state to hold new Movie data

--- client/src/Movies/MovieCreate.js
+++ client/src/Movies/MovieCreate.js
@@ -3,8 +3,12 @@ import React from "react";
 export default class MovieCreate extends React.Component {
 	constructor(props) {
 		super();
+		// hold new movie data here
 		this.state = {
-
+			title: "",
+			director: "",
+			metascore: "",
+			stars: []
 		}
 	}
 
